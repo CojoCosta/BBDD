30d4ea2b632fae358f3febf81133179d89ac070c
CojoCosta <dcosta1882000@gmail.com>
2025-04-08 12:00:01 +0200

    changes

--- ConsultasTema9.sql
+++ ConsultasTema9.sql
@@ -1,41 +1,70 @@
-2 / 4 / 7 / 8 / 9 / 10 / 11 / 15 / 18 / 19 / 20 / 22 / 23 / 25 / 27 / 28 / 30
--- 1. Lista los usuarios dados de alta en el servidor junto con el equipo desde donde se
--- pueden conectar
-
--- 2. Crea el usuario user1 con clave pass y que pueda conectarse desde la máquina local.
-    CREATE USER user1@localhost IDENTIFY BY 'pass';
-
--- 4. Otorga con el usuario user1@localhost permisos de consulta al usuario user1@localhost sobre la tabla emple de la base de datos tema8. ¿Qué ha pasado y por qué?¿Como lo solucionarías?. Solucionalo.
-
--- 7. Cambia la contraseña al usuario user1@localhost a 'password' y reconéctate.
-    SET PASSWORD FOR user1@localhost = PASSWORD('password');
-
--- 8. Otorga privilegios de consulta a la columna nombre y loc de la taba depart para el usuario user1 desde la maquina local.
+2. CREATE USER user1@localhost IDENTIFIED BY 'pass';
 
 
--- 9. Consulta todos los datos disponibles en depart. ¿Qué ha pasado?. Consulta solo los datos a los que se está autorizado. ¿Qué sucede?
-    
+4. GRANT SELECT ON tema5.empleados TO user1@localhost;
+-- Hay que darle permisos de consulta al usuario user1
+-- desde el usuario root
 
--- 10.Crea de forma simultánea un usuario, user2, con clave pass2 y que solo puede acceder desde la máquina local y un usuario user3 que puede acceder desde el equipo de un compañero de clase con clave pass3
 
--- 11. Permite que user2 tenga todos los privilegios sobre la base de datos tema8 y que user3 tenga los privilegios de consulta e inserción sobre la tabla depart.
+7. SET PASSWORD FOR user1@localhost = PASSWORD('password');
 
--- 15.Conéctate a la base de datos de tu compañero e inserta una fila en su tabla depart y es su tabla emple. ¿Qué sucede?
 
--- 18.Renombra los usuarios user2 y user3 a usuario2 y usuario3 con sus correspondientes hosts. Muestra los usuarios de MySQL y comprueba que los cambios se han realizado de forma correcta.
+8. GRANT SELECT(nombre, loc) ON TEMA5.depart TO user1@localhost;
 
--- 19. Elimina del usuario3 (antes user3) que se conecta desde el equipo de tu compañero la posibilidad de consultar la vista vista.
 
--- 20.Borra este usuario y consulta la tabla de ususarios
+9.  
+-- Informa del error de no tener permisos en la columna iddepart.
+-- Al consultar solo los permitidos si deja y salen por pantalla.
 
--- 22.Añade a usuario2 desde el equipo local la posibilidad de borrar y crear tablas en la base de datos tema8.
 
--- 23. Para el usuario2 desde el equipo local, permite que pueda actualizar el valor de la columna jefe de la tabla empleados e insertar en comisión.
+10.
+CREATE USER user2@localhost IDENTIFIED BY 'pass2';
+CREATE USER user3@"192*" IDENTIFIED BY 'pass3';
 
--- 25.Actualiza el código de jefe a 2 para aquellos cuyo codigo de jefe sea uno. ¿Qué ha sucedido?
 
--- 27. Limita al user1 a tener como mucho dos conexiones simultáneas y comprueba que no se pueden establecer tres conexiones simultáneas.
+11.
+GRANT ALL ON tema8.* TO user2@localhost WITH GRANT OPTION;
+GRANT ALL ON tema5.depart TO user3@"192*";
 
--- 28. Limita al user1 a poder realizar como mucho 10 consultas a la hora y compruébalo.
 
--- 30.Actualiza los privilegios
+15.
+-- No permite hacerlo en la tabla empleados por los permisos
+
+
+18.
+RENAME USER user2@localhost TO usuario2@localhost IDENTIFIED BY 'pass2'
+RENAME USER user3@'192*' TO usuario3@'192*' IDENTIFIED BY 'pass3'
+
+
+19.
+REVOKE SELECT ON *.* TO usuario3@'192*';
+
+
+20.
+DROP USER usuario3@'192*';
+
+
+22.
+GRANT CREATE, DELETE ON tema8.* TO usuario2@localhost;
+
+
+23.
+GRANT UPDATE(jefe), INSERT(comision) ON tema5.empleados
+TO usuario2@localhost
+
+
+25.
+UPDATE empleados.JEFE SET JEFE=2 WHERE JEFE=1;
+-- UPDATE comando negado para usuario: 'usuarioo2'@'localhost' para tabla `empleados`.`jefe`
+
+
+27.
+CREATE OR REPLACE // ALTER USER user1@localhost IDENTIFIED BY pass WITH MAX_USER_CONNECTIONS_PER_HOUR 2;
+
+
+28.
+CREATE OR REPLACE // ALTER USER user1@localhost IDENTIFIED BY pass WITH
+MAX_QUERIES_PER_HOUR 10;
+
+30.
+FLUSH PRIVILEGES;
