45ab44368a3dbca939cb6a2b5364d8ad419b5c64
CojoCosta <dcosta1882000@gmail.com>
2025-05-14 11:20:09 +0200

    changes

--- ConsultasTema10.sql
+++ ConsultasTema10.sql
@@ -10,9 +10,10 @@ DELIMITER ;
 
 -- 2. Crea una función que pasándole una cadena de texto nos devuelva la cadena concatenada con un guion y su longitud (por ejemplo, con la cadena 'hola' devolverá 'hola - 4'). Tendrá que tener el comentario: "Mi primer procedimiento almacenado".
 DELIMITER $
-CREATE FUNCTION cadena(cadena string) RETURNS string
+CREATE FUNCTION cadena(cadena VARCHAR(255)) RETURNS cadena VARCHAR(255) 
+COMMENT "Mi primer procedimiento almacenado"
 BEGIN
-RETURN cadena + ' - ' + length(cadena);
+RETURN CONCAT(cadena, ' - ', length(cadena));
 END $
 DELIMITER ;
 
@@ -25,7 +26,7 @@ END $
 DELIMITER ;
 
 -- 5. Muestra todos los procedimientos creados.
-SHOW PROCEDURE STATUS LIKE '%';
+SHOW PROCEDURE STATUS;
 
 -- 9. Muestra el código de creación del procedimiento prueba.
 SHOW CREATE PROCEDURE prueba;
@@ -36,14 +37,52 @@ ALTER PROCEDURE prueba  SQL SECURITY DEFINER COMMENT 'Procedimiento de prueba';
 
 
 -- 14. Crear un procedimiento que liste un número (pasado como parámetro ) de filas de la tabla emple. El procedimiento se ha de ejecutar con los permisos del usuario que lo invoca.
+DELIMITER $
+CREATE PROCEDURE listar(IN numFilas int)
+BEGIN
+SELECT * FROM empleados ORDER BY apellidos LIMIT numFilas;
+END $
+DELIMITER ;
 
+--15. Crea un procedimiento que devuelva, como parámetro, el máximo salario de la tabla emple.
+DELIMITER $
+CREATE PROCEDURE SalarioMaximo(OUT maxSalario int)
+BEGIN
+SELECT MAX(SALARIO) FROM empleados ORDER BY SALARIO DESC LIMIT 1;
+END $
+DELIMITER ;
 
 
 -- 16. Muestra un procedimiento que devuelva, como parámetro, el número de departamentos distintos hay en la tabla emple. El procedimiento se ha de ejecutar con los permisos del usuario que lo crea
+DELIMITER $
+CREATE PROCEDURE muestraDepart(OUT numDepart int)
+BEGIN
+SELECT COUNT(DISTINCT nombre) FROM depart;
+END $
+DELIMITER ;
+
+-- 17. Crea un procedimiento para insertar un nuevo alumno en la tabla alumnos. El procedimiento se ha de ejecutar con los permisos del usuario que lo invoca.
+
+
+-- 18. Crea una función que pasándole un oficio nos devuelva el número de empleados con ese oficio. Se ha de crear el procedimiento para el usuario root conectado desde la maquina local.
+
+
+-- 19. Crea un procedimiento, que pasándole un oficio, nos devuelva el número de empleados con ese oficio.
+
+-- 21. Crea un procedimiento que busque en la tabla empleados todos los empleados en que sus apellidos cumpla con un patrón.
+
+
+-- 22. Crea un procedimiento que indicado un empleado le aumente la comisión en un cifra determinada.
+
+
+-- 23. Crea un función que dado un empleado calcule el número de años que lleva en la empresa.
+
 
 -- 20. Vamos a crear una función determinista que califica el salario de los empleados  con una cadena de texto: si el salario es superior o igual 5000 Alto, si es mayor o igual que 3000 pero menor 5000 Medio y bajo en los demás casos. Luego de la definición del procedimiento muestra los datos de los empleados con su salario calificado.
 
+
 -- 24. Crea un procedimiento que busque en la tabla empleados todos los empleados en que sus apellidos cumpla con un patrón. Además deberá mostrar el número de empleados que cumple el patrón. Ha de tener el comentario de "busca procedimiento".
+
 
 -- 26. Crea un procedimiento que indicándole un código de empleado nos devuelva el tipo de salario: El cual puede ser bajo si cobra menos de 2000, medio si cobra 2000 o más pero menos de 4000 y alto el resto. El procedimiento se ha de ejecutar con los permisos del usuario que lo invoca y se debe crear para el  usuario user desde la maquina local.
 
